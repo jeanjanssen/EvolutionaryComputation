03297efebeac0c81186ddfc3d15c433c14bc90b1
jeanjanssen <jean.janssen2000@gmail.com>
2022-02-22 19:52:22 +0100

    Push

---
+++ GenAlgo.py
@@ -0,0 +1,4 @@
+class GenAlgo:
+
+    def __init__(self):
+        self.pop = Knapsack.initial_pop()
---
+++ Knapsack.py
@@ -0,0 +1,21 @@
+import numpy as np
+import math
+
+class Knapsack:
+    index = np.arange(1, 10)
+    weight = np.randint(1, 10)
+    max_weight = 25
+    value = index
+
+    solutions = 8
+    pop_size = (solutions, index.shape[0])
+
+    def gen_pop(pop_size):
+        initial_pop = np.random.randint(2, size=pop_size)
+        initial_pop = initial_pop.astype(int)
+        return initial_pop
+
+    num_generations = 50;
+
+if __name__ == '__main__':
+
